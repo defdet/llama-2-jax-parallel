9ee47795e1dfe17fc813f08179463c26178be57b
Max <bolgov1458@yandex.ru>
2024-02-25 01:55:10 +0300

    add 2-d parallel

--- lib/multihost_utils/shard_array.py
+++ lib/multihost_utils/shard_array.py
@@ -4,21 +4,29 @@ import jax
 from jax import Array
 from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
 import numpy as np
+import jax.experimental.mesh_utils as mesh_utils
 
-def shard_array(arr: Array, axis: int | EllipsisType) -> Array:
+def shard_array(arr: Array, axes: tuple | EllipsisType) -> Array:
+    if len(axes) == 2:
+        device_tuple = (2, 8)            
+    elif len(axes)==1:
+        device_tuple = (2, 2, 4)
+    else:
+        device_tuple = (16)
+    
+    devices = mesh_utils.create_device_mesh(device_tuple)
     shape = arr.shape
-    devices: np.ndarray = np.array(jax.devices())
 
-    if axis is ...:
+    if axes is ...:
         mesh = Mesh(devices, ('a',))
         sharding = NamedSharding(mesh, P(None))
     else:
         sharding_tuple_ = [1] * len(shape)
-        sharding_tuple_[axis] = -1
+        for axis_num, axis in enumerate(axes):
+            sharding_tuple_[axis]=device_tuple[axis_num]
         sharding_tuple = tuple(sharding_tuple_)
         name_tuple = tuple('abcdefghijklmnopqrstuvwxyz'[:len(shape)])
-
-        mesh = Mesh(devices.reshape(sharding_tuple), name_tuple)
+        mesh = Mesh(devices.reshape(sharding_tuple), name_tuple)     
         sharding = NamedSharding(mesh, P(*name_tuple))
 
     xs = [jax.device_put(arr[i], device) for device, i in sharding.addressable_devices_indices_map(shape).items()]
