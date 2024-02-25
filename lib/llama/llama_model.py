46613e66d1ef8c815c47e701b594bebd9218c26b
Max <bolgov1458@yandex.ru>
2024-02-25 14:32:55 +0300

    Update llama_model.py

--- lib/llama/llama_model.py
+++ lib/llama/llama_model.py
@@ -12,6 +12,8 @@ from .embedding import check_embedding, forward_embedding, init_embedding
 from .kv_cache import KVCache
 from .rms_norm import check_rms_norm, forward_rms_norm, init_rms_norm
 from .rotary_embedding import RotaryValues
+from jax.experimental import mesh_utils
+from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
 
 class LlamaModel(NamedTuple):
     embedding: Any  # Array
@@ -42,8 +44,25 @@ def forward_llama_model(params: LlamaModel, seq: Array, qk_mask: Array, *, rotar
     assert qk_mask.dtype == jnp.bool_
     assert model_config.d_k % 2 == 0
     assert key is None or model_config.dropout_rate is not None
+    devices = mesh_utils.create_device_mesh((16, ))
+    device_tuple = (2, 8)
+
+    seq_axes = (0, 2)
+
+    sharding_tuple_seq = [1] * 3
+
+    for axis_num, axis in enumerate(seq_axes):
+        sharding_tuple_seq[axis]=device_tuple[axis_num]
+
+    sharding_tuple_seq = tuple(sharding_tuple_seq)
+
+    name_tuple_seq = tuple('abcdefghijklmnopqrstuvwxyz'[:3])
+    mesh_seq = Mesh(devices.reshape(sharding_tuple_seq), name_tuple_seq)     
+    sharding_seq = NamedSharding(mesh_seq, P(*name_tuple_seq))
 
     seq = forward_embedding(params.embedding, seq)
+    seq = jax.lax.with_sharding_constraint(seq, sharding_seq)
+
     seq, kv_cache = forward_decoder(params.decoder, seq, qk_mask, rotary_values=rotary_values, kv_cache=kv_cache, key=key, model_config=model_config)
     seq = forward_rms_norm(params.norm, seq, model_config=model_config)
     return seq, kv_cache
