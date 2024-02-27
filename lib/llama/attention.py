b3b7f8b34c601c6d68b0780ca9c5499329337475
Max <bolgov1458@yandex.ru>
2024-02-27 21:20:25 +0300

    use qwen 1.8B

--- lib/llama/attention.py
+++ lib/llama/attention.py
@@ -106,7 +106,8 @@ def forward_attention(params: Attention, src_seq: Array, dst_seq: Array, qk_mask
         k = k_cache.at[:, :, -1:].set(k)
         v = v_cache.at[:, :, -1:].set(v)
 
-    q = q.reshape(q.shape[0], model_config.n_rep_kv * model_config.n_heads_kv, q.shape[3], model_config.d_k)
+    # q = q.reshape(q.shape[0], model_config.n_rep_kv * model_config.n_heads_kv, q.shape[3], model_config.d_k)
+    q = q.reshape(q.shape[0], 16, q.shape[3], model_config.d_k) # For qwen 1.8 B
     qk_mask = qk_mask.squeeze(1)
     qk_mask = jnp.broadcast_to(qk_mask, (qk_mask.shape[0], model_config.n_rep_kv * model_config.n_heads_kv, qk_mask.shape[3], qk_mask.shape[2]))
 
