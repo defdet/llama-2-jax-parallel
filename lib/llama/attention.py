00779b25dc9800e7b626cfa584bd05998774e760
Max <bolgov1458@yandex.ru>
2024-02-27 18:58:00 +0300

    Update attention.py

--- lib/llama/attention.py
+++ lib/llama/attention.py
@@ -118,7 +118,7 @@ def forward_attention(params: Attention, src_seq: Array, dst_seq: Array, qk_mask
                    P(*name_tuple_k),
                    P(*name_tuple_k))
     
-    qkv = shard_map(partial(flash_attention.flash_attention, sm_scale=math.sqrt(model_config.d_k), debug=False, causal=True), mesh=mesh_k, in_specs=specs_tuple, out_specs=P(*name_tuple_k), check_rep=False)(q, k, v, None)
+    qkv = shard_map(partial(flash_attention.flash_attention, sm_scale=math.sqrt(model_config.d_k), debug=False, causal=True), mesh=mesh_k, in_specs=specs_tuple, out_specs=P(*name_tuple_k), check_rep=False)(q, k, v)
     jax.debug.print("🤯 {x} 🤯", x=qkv)
     qkv = jnp.expand_dims(qkv, 1)
     qkv = qkv.astype(jnp.bfloat16)
