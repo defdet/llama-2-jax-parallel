a72fe85a82283d5a8e76efbe05e8c2a550d7d677
Max <bolgov1458@yandex.ru>
2024-03-01 08:30:31 +0300

    Update attention.py

--- lib/llama/attention.py
+++ lib/llama/attention.py
@@ -172,7 +172,7 @@ def forward_attention(params: Attention, src_seq: Array, dst_seq: Array, qk_mask
                    P(*name_tuple_k))
     
     if attn_impl == 'flash':
-        qkv = shard_map(partial(flash_attention, sm_scale=math.sqrt(model_config.d_k), debug=False, causal=False, block_sizes=block_sizes), mesh=mesh_k, in_specs=specs_tuple, out_specs=P(*name_tuple_k), check_rep=False)(q, k, v, None)
+        qkv = shard_map(partial(flash_attention, sm_scale=math.sqrt(model_config.d_k), debug=False, causal=False, block_sizes=block_sizes), mesh=mesh_k, in_specs=specs_tuple, out_specs=P(*name_tuple_k), check_rep=False)(q, k, v, attention_bias)
     elif attn_impl == 'ring':
         qkv = shard_map(partial(ring_attention, sm_scale=math.sqrt(model_config.d_k), debug=False, causal=True), mesh=mesh_k, in_specs=specs_tuple, out_specs=P(*name_tuple_k), check_rep=False)(q, k, v, attention_bias)
     print(qkv.shape)
